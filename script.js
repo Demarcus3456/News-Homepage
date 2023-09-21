806edf0aa4d9bb3321b7536e7b272f542d091980
“Demarcus <dcolemand@ymail.com>
2023-09-21 16:07:48 -0500

    Mobile settings
    Added a basic mobile layout

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict'
 
+const openMenu = document.querySelector('.menu-open');
+const closeMenu = document.querySelector('.menu-close');
+const menu = document.querySelector('.nav');
+
+// Open menu
+openMenu.addEventListener('click', function () {
+    menu.classList.remove('hidden');
+    openMenu.classList.add('hidden');
+});
+
+// Close menu
+closeMenu.addEventListener('click', function () {
+    menu.classList.add('hidden');
+});
